5001a10a8b0006258475e68cc33c3e39f42b44f5
CarlosAlmeida1 <carloshenriquealmeida52@gmail.com>
2024-04-15 21:42:10 -0300

    chore: adicionando registro de usuario e começando a estrutura do projeto com controllers e services

---
+++ src/http/controllers/register.controller.ts
@@ -0,0 +1,34 @@
+import z from "zod";
+import { RegisterService } from "@/services/register";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/services/errors/user-already-exists";
+
+export async function register(req: FastifyRequest, reply: FastifyReply) {
+  const registerUserBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerUserBodySchema.parse(req.body);
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerService = new RegisterService(prismaUsersRepository);
+
+    await registerService.execute({
+      name,
+      email,
+      password,
+    });
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+
+    throw error
+  }
+
+  return reply.status(201).send();
+}
