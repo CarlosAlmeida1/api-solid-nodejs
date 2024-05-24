ecbc71ea5fb1373a16c082943f9560fc08fc1413
Carlos Henrique de Almeida Martins <carloshenriquealmeida52@gmail.com>
2024-05-24 14:54:02 -0300

    feat: adicionando teste e2e de autenticação

--- src/http/controllers/authenticate.test.ts
+++ src/http/controllers/authenticate.test.ts
@@ -1,3 +1,31 @@
-import { test } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
 
-test("ok", () => {});
+describe("Authenticate (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test("should be able to authenticate", async () => {
+    await request(app.server).post("/users").send({
+      name: "Carlos Henrique",
+      email: "carlos@gmail.com",
+      password: "123456",
+    });
+
+    const response = await request(app.server).post("/sessions").send({
+      email: "carlos@gmail.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    });
+  });
+});
