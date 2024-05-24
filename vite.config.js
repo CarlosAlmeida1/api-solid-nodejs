465e12063e3776353ebd7cade3fc10af12150887
Carlos Henrique de Almeida Martins <carloshenriquealmeida52@gmail.com>
2024-05-23 23:02:56 -0300

    adicionando configurações de teste E2E

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
-
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [
+      [
+        "src/http/controllers/**",
+        "./prisma-test-environment/prisma-test-environment.ts",
+      ],
+    ],
+    dir: "src",
+  },
 });
+
+//     "./prisma-test-envirnoment/prisma-test-environment.ts",
