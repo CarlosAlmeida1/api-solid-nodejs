d155167398e55e6e3004d276289e43944d023056
Carlos Henrique de Almeida Martins <carloshenriquealmeida52@gmail.com>
2024-05-26 23:00:43 -0300

    feat: Adicionar rotas de busca e criação de academias

---
+++ src/http/controllers/gyms/search.controller.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { makeSearchGymsService } from "@/services/factories/make-search-gyms-service";
+
+export async function search(req: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { page, q } = searchGymsQuerySchema.parse(req.body);
+
+  const searchGymService = makeSearchGymsService();
+
+  const { gyms } = await searchGymService.execute({
+    page,
+    query: q,
+  });
+
+  return reply.status(201).send({
+    gyms,
+  });
+}
